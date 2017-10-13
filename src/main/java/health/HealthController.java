0d8bedcb8cc5b2d5bf603d7e9a192d666495fdb8
Ashley Roach <asroach@cisco.com>
2017-10-13 16:39:43 -0600

    Refactored per Ali's feedback

--- src/main/java/health/HealthController.java
+++ src/main/java/health/HealthController.java
@@ -2,7 +2,11 @@ package health;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import models.HealthResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -12,31 +16,37 @@ import java.util.logging.Logger;
 @RestController
 public class HealthController {
 
-    private static final Logger log = Logger.getLogger(Health.class.getName());
+    private static final Logger log = Logger.getLogger(HealthController.class.getName());
 
-    @RequestMapping("/health")
-    public void health(HttpServletResponse response) throws IOException{
+    @RequestMapping(method = RequestMethod.GET, value = "/health", produces = "application/json")
+    public ResponseEntity<HealthResponse> health() {
 
         log.info("Health endpoint triggered");
 
-        HttpResponse<JsonNode> result = new Health().getHealth();
+        HealthResponse response = new HealthResponse();
 
-        if (result.getBody()
-                .getObject()
-                .getJSONObject("response")
-                .getJSONObject("request")
-                .getString("status").equals("FAILED")) {
-
-            log.info("Health check failed: Sending response");
-
-            response.sendError(500, result.getBody().toString());
-        } else {
-
-            log.info("Health check passed: Sending response");
-
-            response.setStatus(200);
-
-        }
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+////        HttpResponse<JsonNode> result = response.getHealth();
+//
+//        if (response.getBody()
+//                .getObject()
+//                .getJSONObject("response")
+//                .getJSONObject("request")
+//                .getString("status").equals("FAILED")) {
+//
+//            log.info("Health check failed: Sending response");
+//
+////            response.sendError(500, result.getBody().toString());
+//            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+//
+//        } else {
+//
+//            log.info("Health check passed: Sending response");
+//
+////            response.setStatus(200);
+//            return new ResponseEntity<>(response, HttpStatus.OK);
+//
+//        }
     }
 
 }
