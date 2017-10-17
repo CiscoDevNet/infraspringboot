8970e1c95591a591cb3b7cb44e49d01d5f89bef0
Ali Moghadam <amoghada@cisco.com>
2017-10-17 14:15:36 -0700

    Adding Custom Response

--- src/main/java/com/cisco/devnet/example/configs/AppConfig.java
+++ src/main/java/com/cisco/devnet/example/configs/AppConfig.java
@@ -1,13 +1,8 @@
 package com.cisco.devnet.example.configs;
 
 import com.cisco.devnet.infracheck.InfraCheck;
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 public class AppConfig {
@@ -17,15 +12,15 @@ public class AppConfig {
         return new InfraCheck();
     }
 
-    @Bean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
-        jsonConverter.setObjectMapper(objectMapper);
-
-        return jsonConverter;
-    }
+//    @Bean
+//    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+//        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//        objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+//        jsonConverter.setObjectMapper(objectMapper);
+//
+//        return jsonConverter;
+//    }
 }
--- src/main/java/com/cisco/devnet/example/indicators/InfraCheckHealthIndicator.java
+++ src/main/java/com/cisco/devnet/example/indicators/InfraCheckHealthIndicator.java
@@ -1,14 +1,20 @@
 package com.cisco.devnet.example.indicators;
 
 import com.cisco.devnet.infracheck.InfraCheck;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class InfraCheckHealthIndicator implements HealthIndicator {
@@ -39,10 +45,44 @@ public class InfraCheckHealthIndicator implements HealthIndicator {
                 .getJSONObject("request")
                 .getString("status");
 
+        ObjectMapper mapper = new ObjectMapper();
+
         if (healthStatus.equals("FAILED")) {
-            return Health.down().withDetail("Error Code", response.getBody().getObject()).build();
+
+            Result result;
+            try {
+                result = mapper.readValue(response.getBody().getObject().toString(), Result.class);
+            } catch (IOException e) {
+                throw new RuntimeException("ERROR");
+            }
+
+            return Health.down().withDetail("Error Code", result).build();
         }
         return Health.up().build();
     }
 
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Result {
+
+        @JsonProperty("response")
+        private Response response;
+
+        @Data
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class Response {
+
+            @JsonProperty("request")
+            private Request request;
+
+            @Data
+            @JsonIgnoreProperties(ignoreUnknown = true)
+            public static class Request {
+
+                @JsonProperty("destIP")
+                private String destIP;
+
+            }
+        }
+    }
 }
