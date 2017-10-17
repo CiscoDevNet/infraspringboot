a471550921f66da3fbbac907933a1db1c09d2a0d
Ashley Roach <asroach@cisco.com>
2017-10-17 11:22:58 -0600

    Updates to use more idiomatic spring

--- src/main/java/com/cisco/devnet/example/Application.java
+++ src/main/java/com/cisco/devnet/example/Application.java
@@ -3,9 +3,6 @@ package com.cisco.devnet.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Added componentscan when doing configuration
-// https://stackoverflow.com/questions/40384056/consider-defining-a-bean-of-type-package-in-your-configuration-spring-boot#comment73086009_40388609
-
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/cisco/devnet/example/indicators/InfraCheckHealthIndicator.java
+++ src/main/java/com/cisco/devnet/example/indicators/InfraCheckHealthIndicator.java
@@ -1,7 +1,8 @@
 package com.cisco.devnet.example.indicators;
 
-import com.cisco.devnet.example.models.HealthResponse;
 import com.cisco.devnet.infracheck.InfraCheck;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,10 +29,13 @@ public class InfraCheckHealthIndicator implements HealthIndicator {
 
         LOGGER.info(String.format("TraceID is equal to %s", traceId));
 
-        HealthResponse response = new HealthResponse();
+        String ticket = infraCheck.getTicket();
+        HttpResponse<JsonNode> response = infraCheck.pathCheck(ticket, traceId);
 
-        if (response.getStatus().equals("FAILED")) {
-            return Health.down().withDetail("Error Code", response.getOriginalResponse()).build();
+        String healthStatus = response.getBody().getObject().getJSONObject("response").getJSONObject("request").getString("status");
+
+        if (healthStatus.equals("FAILED")) {
+            return Health.down().withDetail("Error Code", response.getBody().toString()).build();
         }
         return Health.up().build();
     }
